9a5b455b53091ea0610ed0ebe8e68a6c136421dc
Martin Jimenez <34499879+maferjramirez@users.noreply.github.com>
2025-10-19 13:14:36 -0600

    Modify microPc to handle 4-bit input and output and increment +1

--- microPc.vhd
+++ microPc.vhd
@@ -2,22 +2,24 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
 ENTITY microPc IS
-    PORT (
-        clk : IN STD_LOGIC;
-        reset : IN STD_LOGIC;
-        D : IN STD_LOGIC_VECTOR (5 DOWNTO 0);
-        Q : OUT STD_LOGIC_VECTOR (5 DOWNTO 0));
+	PORT (
+		clk : IN STD_LOGIC;
+		reset : IN STD_LOGIC;
+		D : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+		Q : OUT STD_LOGIC_VECTOR (3 DOWNTO 0)
+	);
 END microPc;
 ARCHITECTURE Behavioral OF microPc IS
-    CONSTANT s0 : STD_LOGIC_VECTOR(5 DOWNTO 0) := B"000000";
+	CONSTANT s0 : STD_LOGIC_VECTOR(3 DOWNTO 0) := B"0000";
 BEGIN
-    PROCESS (clk, reset)
-    BEGIN
-        IF reset = '0' THEN
-            Q <= s0;
-        ELSIF rising_edge (clk) THEN
-            Q <= D;
-        END IF;
-    END PROCESS;
+	PROCESS (clk, reset)
+	BEGIN
+		IF reset = '0' THEN
+			Q <= s0;
+		ELSIF rising_edge (clk) THEN
+			Q <= D + 1;
+		END IF;
+	END PROCESS;
 END Behavioral;
