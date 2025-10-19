3c6086062724a5c4108c6ea159dc32aab3d02f0c
Martin Jimenez <34499879+maferjramirez@users.noreply.github.com>
2025-10-19 13:13:25 -0600

    Refactor logica_secuenciador

--- logica_secuencidor.vhd
+++ logica_secuencidor.vhd
@@ -1,49 +1,49 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity logica_secuencidor is
-    port(
-        I           : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-        CC          : IN std_logic;
-        selector    : IN std_logic;
-        PL_liga     : OUT std_logic;
-        MAP_transf  : OUT std_logic;
-        VECT_int    : OUT std_logic;
-    )
-end logica_secuencidor;
+ENTITY logica_secuenciador IS
+	PORT (
+		CC : IN std_logic;
+		Ins : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+		selector : OUT std_logic;
+		PL_liga : OUT std_logic;
+		MAP_transf : OUT std_logic;
+		VECT_int : OUT std_logic
+	);
+END logica_secuenciador;
 
-architecture Behavioral of logica_secuenciador is
-    begin
-        process(I, CC)
-        begin
-            selector <= '0';
-            PL_liga <= '1';
-            MAP_transf <= '1';
-            VECT_int <= '1';
-        
+ARCHITECTURE Behavioral OF logica_secuenciador IS
+BEGIN
+	PROCESS (Ins, CC)
+	BEGIN
+		selector <= '0';
+		PL_liga <= '1';
+		MAP_transf <= '1';
+		VECT_int <= '1';
+ 
 
-        case I is
-            when "00" =>
-                null; -- Usamos los valores por default
-            
-            when "01" =>
-                if CC = '0' then
-                    selector <= '1'; -- Salto Condicional: Y=D
-                    PL_liga <= '0';
-                    -- MAP y VECT se quedan en 1
-                end if;
-            
-            when "10" => -- Salto de transformación: Y=D, Activamos MAP
-                selector <= '1';
-                MAP_transf <= '0';
-            
-            when others => -- 11 Salto condicional de interrupción: Y=D
-                if CC = '0' then
-                    selector <= '1';
-                    VECT_int <= '0';
-                end if ;
-        end case;
-    end process;
-end architecture;
+		CASE Ins IS
+			WHEN "00" => 
+				NULL; -- Usamos los valores por default
+ 
+			WHEN "01" => 
+				IF CC = '0' THEN
+					selector <= '1'; -- Salto Condicional: Y=D
+					PL_liga <= '0';
+					-- MAP y VECT se quedan en 1
+				END IF;
+ 
+			WHEN "10" => -- Salto de transformación: Y=D, Activamos MAP
+				selector <= '1';
+				MAP_transf <= '0';
+ 
+			WHEN OTHERS => -- 11 Salto condicional de interrupción: Y=D
+				IF CC = '0' THEN
+					selector <= '1';
+					VECT_int <= '0';
+				END IF;
+		END CASE;
+	END PROCESS;
+END ARCHITECTURE;
